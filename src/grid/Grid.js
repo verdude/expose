0cad801b3504707b029f14b66a3274b125743244
Santiago Verdu <santiago_verdu@byu.edu>
2019-03-10 17:23:04 -0600

    remove slideritem completely

--- src/grid/Grid.js
+++ src/grid/Grid.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-import SliderItem from '../slider/SliderItem'
 import { images } from '../imgs'
 import Img from '../image/Img'
 
