cfa29a5a11719d49eefe778ca9a8fbf477187538
Santiago Verdu <santiago_verdu@byu.edu>
2019-03-10 18:10:41 -0600

    Image decorations

--- src/image/Img.js
+++ src/image/Img.js
@@ -12,6 +12,11 @@ const Container = styled.div`
         font-weight: 500;
         text-align: center;
     }
+
+    & div {
+        box-shadow: 7px 7px 5px #aaaaaa;
+        border-radius: 3px;
+    }
 `
 
 class Picture extends Component {
